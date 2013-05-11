63a16e10046618803e1ca683474975786ea618dd
Linus Lilja <liljalinus@gmail.com>
2013-05-11 18:36:05 +0200

    Small fix

--- include/header.php
+++ include/header.php
@@ -130,66 +130,8 @@ Crew at Reesume';
   
   <link rel="canonical" href="http://reesu.me/" />
   <link rel="image_src" href="http://reesu.me/asset/img/img_src.png" />
-
-	<style type="text/css">
-		@import url(http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600);
-		@import url(http://fonts.googleapis.com/css?family=Dancing+Script);
-
-		* { margin: 0; padding: 0; -webkit-font-smoothing: antialiased; }
-		a:hover, a.logo:hover, ul#nav > li:hover:first-of-type:after { -webkit-transition: all 0.2s ease-in; -moz-transition: all 0.2s ease-in; -o-transition: all 0.2s ease-in; transition: all 0.2s ease-in; }
-		.clear { clear: both; font-size: 1px; }
-		a, a:visited { color: #FAFAFA; text-decoration: none; }
-		a:hover { text-decoration: none; }
-
-		html, body { color: #4D4D4D; background: url(http://styleblacks.com/wp-content/uploads/2013/04/Wood-texture-background-1920-1200-resolution-in-wallpapers-hd.jpg) fixed center center no-repeat; font-family: 'Source Sans Pro', sans-serif; line-height: 1.5; font-size: 1.1em; height: 100%; }
-		
-		#headings { max-width: 800px; width: calc(80% + 60px); margin: 0 auto; min-height: 100px; max-height: 150px; }
-		a.logo  { background: #10D080;float: left; text-indent: 20px; width: 100px; height: 100px; line-height: 100px; font-size: 3em; color: #FFF; font-family: "Dancing Script", sans-serif; } 
-		a.logo > h1 span { display: none; }
-		a.logo:hover { color: rgba(255,255,255, .8); _color: #EEE; }
-		
-		ul#nav { float: left; position: relative; }
-		ul#nav li { list-style-type: none; display: block; background: #393c3a; height: 50px; width: 150px; display: block; }
-		ul#nav li:hover { background: #2B2D2B; }
-		ul#nav > li:first-of-type { width: 50px; float: left; }
-		ul#nav > li:first-of-type:after { padding: 15px 20px; line-height: 50px; color: #FFF; content: '\25BC'; border: 0; }
-		ul#nav > li:hover:first-of-type:after { content: '\25b2'; }
-
-		ul#nav li > ul { display: none; position: absolute; z-index: 100; top: 50px; width: 150px; }
-		ul#nav li:hover > ul { display: block; }
-		ul#nav li > ul li { height: 50px; width: 150px; text-transform: uppercase; }
-		ul#nav li > ul li a, ul#nav li > ul li a:visited { line-height: 50px; display: inline-block; padding: 0 20px; }
-		ul#nav li > ul li:hover { background: #2B2D2B; }
-		ul#nav li > ul li a > span { color: #D06868; font-size: .6em; font-weight: 500; }
-		
-		.notification { height: 50px; float: left; line-height: 50px; color: #fff; font-style: italic; text-indent: 20px; }
-		.notification a, .notification a:visited { text-decoration: underline; color: #fff; }
-		.notification a:hover { text-decoration: none; }
-
-		#paper { background: #FAFAFA; position: relative; z-index: 5; padding: 30px; width: 80%; max-width: 740px; margin: 50px auto; }
-		#headings:after, header:after, #paper:after { content: ''; display: block; clear: both; }
-		
-		header { min-height: 150px; }
-		figure.me { display: block; float: right; background: #EEE; top: 75px; position: absolute; top: 50px; right: 50px; width: 120px; height: 120px; -moz-border-radius: 60px; border-radius: 60px; }
-		figure.me span { text-indent: -9999px; background: url('http://new-standards.se/linus/r/img/me.png') no-repeat; display: block; margin: 10px 0 0 10px; background-size: 100px 100px; width: 100px; height: 100px; -moz-border-radius: 50px; border-radius: 50px; }
-		figure span:hover { text-indent: 10px; text-shadow: 0 1px 2px rgba(0,0,0,.3); line-height: 100px; font-size: 1.2em; color: #FFF; filter: grayscale(100%); -webkit-filter: grayscale(100%); -moz-filter: grayscale(100%); -ms-filter: grayscale(100%); -o-filter: grayscale(100%); }
-
-		hgroup > h1 { text-indent: 20px; color: #383838; font-family: "Dancing Script", sans-serif; font-size: 4em; float: left; }
-
-		#resume > .content h3 { color: #383838; }
-		#resume > .content h4 { color: #383838; margin: .5em 0 0 0; font-weight: 600;  }
-		#resume > .content { padding: 10px 20px; margin: 10px 0; }
-		/* Edit mode #resume > .edit:hover { background: #EEE; }*/
-		#resume > .content span { font-style: italic; }
-
-		@media (max-width: 640px) { 
-			figure.me { top: -200%; left: -200%; }
-			hgroup > h1 { font-size: 3.3em; }
-			#headings { width: calc(80% + 30px); } 
-			#paper { padding: 15px; }
-		}
-	</style>
-
+	<link rel="stylesheet" href="assets/css/style.css" media="all" type="text/css" />
+	
   <!--[if lt IE 9]>
     <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
   <![endif]-->
