a47e888a1aa7c6ab6095b0757f86b9dfcaebb57a
Linus Lilja <liljalinus@gmail.com>
2013-05-11 22:36:05 +0200

    Updated files

---
+++ page/reesume.php
@@ -0,0 +1,17 @@
+<section id="paper">
+	<header>
+		<hgroup>
+			<h1>What is Reesume?</h1>
+		</hgroup>
+	</header>
+
+	<section id="resume">
+		<article class="content edit">
+			<h3>Me</h3>
+
+			Enjoys working with productive and goal-oriented individuals. Reliable, social aware, creative and always strive for perfection. 
+			Always do everything wholeheartedly and uncompromising.
+		</article>
+	</section>
+</section>
+<div style="clear: both; font-size: 1px;">&nbsp;</div>
